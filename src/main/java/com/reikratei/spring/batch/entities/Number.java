5db061f47d1c25c303d7e5dd73a77728d64d8084
mreilaender <manuel.reilaender@gmail.com>
2016-12-01 17:26:22 +0100

    first commit

---
+++ src/main/java/com/reikratei/spring/batch/entities/Number.java
@@ -0,0 +1,17 @@
+package com.reikratei.spring.batch.entities;
+
+/**
+ * @author manuel
+ * @version 12/1/16
+ */
+public class Number {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
