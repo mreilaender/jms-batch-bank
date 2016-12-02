e8ee2356aec2d6c668adf78301579a9ec2149015
Kraus <guenter.kraus@accenture.com>
2016-12-02 10:18:14 +0100

    added new step, and finished writer, all works now

--- src/main/java/com/reikratei/spring/batch/processors/NumberProcessor.java
+++ src/main/java/com/reikratei/spring/batch/processors/NumberProcessor.java
@@ -1,29 +1,30 @@
 package com.reikratei.spring.batch.processors;
 
-import com.reikratei.spring.batch.entities.Number;
 import org.springframework.batch.item.ItemProcessor;
+
+import com.reikratei.spring.batch.entities.Number;
 
 /**
  * @author manuel
  * @version 12/1/16
  */
 public class NumberProcessor implements ItemProcessor<Number, Number> {
-    private static Number number = null;
+	private static Number number = null;
 
-    @Override
-    public Number process(Number number) throws Exception {
-        if (NumberProcessor.number == null) {
-            setNumber(number);
-        } else
-            number.add(getNumber());
-        return number;
-    }
+	@Override
+	public Number process(Number number) throws Exception {
+		if (NumberProcessor.number == null) {
+			setNumber(number);
+		} else
+			number.add(getNumber());
+		return number;
+	}
 
-    private static void setNumber(Number number) {
-        NumberProcessor.number = number;
-    }
+	private static void setNumber(Number number) {
+		NumberProcessor.number = number;
+	}
 
-    private static Number getNumber() {
-        return number;
-    }
+	private static Number getNumber() {
+		return number;
+	}
 }
